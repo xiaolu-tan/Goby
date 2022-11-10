9e4d602ae67b89970a731879cbd74853c6dfbfd0
Xiaolu <xiaolutan@apexglobe.com>
2022-11-11 06:00:30 +0800

    fixbug

--- goby_backend/services/users/urls.py
+++ goby_backend/services/users/urls.py
@@ -1,3 +1,11 @@
+'''
+Author: Xiaolu xiaolutan@apexglobe.com
+Date: 2022-10-08 15:56:27
+LastEditors: Xiaolu xiaolutan@apexglobe.com
+LastEditTime: 2022-11-11 05:54:30
+FilePath: /goby_backend/services/users/urls.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 
 from django.urls import path, re_path
 from dj_rest_auth.registration.views import RegisterView, VerifyEmailView,ConfirmEmailView
@@ -18,11 +26,11 @@ urlpatterns = [
     path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
 
     path('profile/experience', WorkExperienceList.as_view()),
-    path('profile/experience/<int:id>', WorkExperienceDetail.as_view()),
+    path('profile/experience/<int:pk>', WorkExperienceDetail.as_view()),
 
     path('profile/education', EducationList.as_view()),
-    path('profile/education/<int:id>', EducationDetail.as_view()),
+    path('profile/education/<int:pk>', EducationDetail.as_view()),
 
     path('profile/project', ProjectList.as_view()),
-    path('profile/project/<int:id>', ProjectDetail.as_view()),
+    path('profile/project/<int:pk>', ProjectDetail.as_view()),
 ]
